08798ad89ed26db6cfa62b5ccaa6f6ffe981d344
Yueh-Han Chen <johneeeeeeeeee3@gmail.com>
2020-03-21 23:53:28 -0700

    Update merge.py

--- merge.py
+++ merge.py
@@ -6,7 +6,7 @@ def days():
 
 if __name__ == '__main__':
 
-    #--------Find game articles--------
+    #--------Find gaming articles--------
     print("From past" ,days() ,"days:")
 
     os.system("python a16z.py")
@@ -14,24 +14,3 @@ if __name__ == '__main__':
     os.system("python Esportobserver.py")
     os.system("python try2.py")
     os.system("python Techcrunch.py")
-
-    #--------Find popular memes------
-
-    # os.system("python IG.py")
-    # os.system("python Dcard.py")
-
-    #--------Play Flow music------
-    #
-    #os.system("python YT_flow.py")
-    #
-    # #--------Play 神人演講 ------
-
-    #os.system("python YT_god.py")
-    #
-    # #--------Find 固定Youtuber的最新影片 ------
-    #
-    #os.system("python Youtubers.py")
-
-
-
-
