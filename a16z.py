ee43cb6b984162f4bf49997b599b3c5c40632cd7
Yueh-Han Chen <johneeeeeeeeee3@gmail.com>
2020-03-30 21:05:30 -0700

    Update a16z.py

--- a16z.py
+++ a16z.py
@@ -28,7 +28,7 @@ def validate_article_date(dom,date_list):
 #function 1: to determine odd month or even month, so we can know the numbers of days
 #function 2: find the date of today
 #find the date of today. If the value minusing 7 is smaller than 0, use find_last_month()
-#把過去第7天的日期+=1 一直加到今天 到一個串列中
+#keep adding 1 to the date constantly. 
 #如果過程中有跨月份，那麼要記得在迴圈中判斷是否到達上個月日期的天數
 
 def give_me_last_several_day_list(days):
